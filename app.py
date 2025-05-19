df5d69f7a14e9974e0a46c4a0a61eeacffd381f7
Ykuroyy <yykm666@gmail.com>
2025-05-19 18:15:20 +0900

    app.pyの変更

--- app.py
+++ app.py
@@ -81,4 +81,5 @@ def predict():
         return jsonify({"error": "商品が見つかりませんでした"}), 404
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
