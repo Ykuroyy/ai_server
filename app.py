92157a0fd7967fce396d7e0798edfde4bd6d529f
Ykuroyy <yykm666@gmail.com>
2025-05-30 14:00:18 +0900

    db追加

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ class ProductMapping(Base):
     id     = Column(Integer, primary_key=True)
     name   = Column(String)
     s3_key = Column(String)
-    # created_at と updated_at カラムを追加
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), server_onupdate=func.now())
+
 
 
 # S3 クライアント
