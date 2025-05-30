76489936c6d7e55510ae76c0ec008d10b44d9762
Ykuroyy <yykm666@gmail.com>
2025-05-30 10:44:22 +0900

    import追記

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import cv2
 from io import BytesIO
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import Column, Integer, String, create_engine
 
 
 
